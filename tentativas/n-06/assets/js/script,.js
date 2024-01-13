5fefda42200792ece9df8caa5944a9d518671846
Daniel <154093698+Daniel-Cafe@users.noreply.github.com>
2024-01-13 11:03:32 -0300

    Initial commit

---
+++ tentativas/n-06/assets/js/script,.js
@@ -0,0 +1,15 @@
+function Mudarestado(el) {
+    var display = document.getElementById(el).style.display;
+    if(display == "none")
+        document.getElementById(el).style.display = 'block';
+    else
+        document.getElementById(el).style.display = 'none';
+  }
+
+  function Mudarestado1(el) {
+    var display = document.getElementById(el).style.display;
+    if(display == "none")
+        document.getElementById(el).style.display = 'block';
+    else
+        document.getElementById(el).style.display = 'none';
+  }
